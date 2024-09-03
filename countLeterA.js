e2ca2be832e1ea85aa7f432f55e0de426fd11cd9
Matheus7p <matheus.costa.7p@gmail.com>
2024-09-03 15:56:09 -0300

    feat: question 2 CountLeterA

---
+++ countLeterA.js
@@ -0,0 +1,19 @@
+// 2) Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula, além de informar a quantidade de vezes em que ela ocorre.
+
+// IMPORTANTE: Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+
+function CountLeterA(string) {
+    const regex = /a/gi;
+    const match = string.match(regex);
+
+    if (!match) {
+        console.log('A letra "a" não foi encontrada na string');
+        return;
+    }
+    console.log(`A letra "a" foi encontrada ${match.length} vezes na string`);
+    
+}
+
+const string = "Eu preciso de um Trabalho!";
+
+CountLeterA(string);
